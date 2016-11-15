5d090923fda4b4f816a14a036d4dac1a198a5992
Martin Wiso <martin@wiso.cz>
2016-11-15 22:13:50 +0100

    Add basic frontend using react/babel/webpack

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ Simple web application to visualize yr.no wether forecast changes in time
 
 Dev run API:
 ```bash
-$ go run main.go
+$ go run yrno.go history.go main.go
 ```
+
 
 # Frontend
 
-Fronted uses [Elm Quickstart][1] project.
-
-Dev run Elm frontend:
+Build frontend:
 ```bash
-$ npm run watch
+$ npm run build
 ```
 
-[1]: https://becoming-functional.com/elm-quickstart-project-73d755ba9b5b#.xy0y0plaz
+Dev fronted server:
+```bash
+$ npm run dev
+```
---
+++ docroot/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  },
+  scripts: {
+    "dev": "webpack -d --watch",
+    "build" : "webpack -p"
+  }
+};
+
+module.exports = config;
